494a063880c80929eb2d7d607b1545e1f074f591
Chandan Taneja <chandan.taneja.931@gmail.com>
2019-09-20 02:04:14 +0530

    Learning Java from Head First

--- DotCom/SimpleDotComGame.java
+++ DotCom/SimpleDotComGame.java
@@ -1,14 +1,19 @@
 package DotCom;
 
+import java.util.ArrayList;
+
 public class SimpleDotComGame {
     public static void main(String[] args) {
         int numOfGuesses = 0;
         GameHelper helper = new GameHelper();
 
-        SimpleDotCom theDotCom = new SimpleDotCom();
-        int randomNum = (int) (Math.random() * 5);
-
-        int[] locations = {randomNum, randomNum+1, randomNum + 2};
+        DotCom theDotCom = new DotCom();
+        int num = (int) (Math.random() * 5);
+        String randomNum = Integer.toString(num);
+        ArrayList<String> locations = new ArrayList<String>();
+        locations.add(randomNum);
+        locations.add(randomNum + 1);
+        locations.add(randomNum + 2);
         theDotCom.setLocationCells(locations);
 
         boolean isAlive = true;
